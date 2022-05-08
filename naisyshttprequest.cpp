2ab540ffefb9a42a2b0933609b25e55931e0472c
Zadock Maloba <malobazadock@gmail.com>
2022-05-08 18:37:42 +0300

    Refactor: Confirm array sizes before looping.

--- naisyshttprequest.cpp
+++ naisyshttprequest.cpp
@@ -6,18 +6,22 @@ NaiSysHttpRequest::NaiSysHttpRequest(const QByteArray &data)
 {
     QJsonObject jsObj;
     auto const _a = QString::fromUtf8(data).split("\r\n\r\n");
-    this->m_rawHeader = _a[0].toUtf8(); this->m_rawBody = _a[1].toUtf8();
-    auto const _b = _a[0].split("\r\n");
-    {
-        auto const _d = _b[0].split(" ");
-        Q_ASSERT(_d.size() == 3);
-        jsObj.insert("Method", _d[0]);
-        jsObj.insert("Path", _d[1]);
-        jsObj.insert("Protocol", _d[2]);
-    }
-    for(int i=0; i<_b.size()-1; ++i){
-        auto const _c = _b[i+1].split(":");
-        jsObj.insert(_c[0], QJsonValue(_c[1]));
+    if(_a.size() >= 2){
+        this->m_rawHeader = _a[0].toUtf8(); this->m_rawBody = _a[1].toUtf8();
+        auto const _b = _a[0].split("\r\n");
+        {
+            auto const _d = _b[0].split(" ");
+            if(_d.size() == 3){
+                jsObj.insert("Method", _d[0]);
+                jsObj.insert("Path", _d[1]);
+                jsObj.insert("Protocol", _d[2]);
+
+                for(int i=0; i<_b.size()-1; ++i){
+                    auto const _c = _b[i+1].split(":");
+                    jsObj.insert(_c[0], QJsonValue(_c[1]));
+                }
+            }
+        }
     }
     m_headerJson = jsObj;
     qDebug() << m_headerJson;
