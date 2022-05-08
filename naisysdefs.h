b836b06ffecf96731661e05d66c23a2b8a05fe77
Zadock Maloba <malobazadock@gmail.com>
2022-05-08 18:35:29 +0300

    Add new objects to the header file.

--- naisysdefs.h
+++ naisysdefs.h
@@ -4,11 +4,24 @@
 #include <QObject>
 #include <QJsonObject>
 #include <QByteArray>
+#include <QSslCertificate>
+#include <QSslKey>
 
 namespace NaiSys {
+
 struct DesirializedData{
     QJsonObject _header;
     QByteArray _body;
+};
+
+struct ServerAddress{
+    int _listen_port = 80;
+    QString _listen_address = "127.0.0.1";
+};
+
+struct SslObject{
+    QSslCertificate *_ssl_cert;
+    QSslKey *_ssl_key;
 };
 }
 
