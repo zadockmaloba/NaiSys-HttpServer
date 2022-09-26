18ec1d48540891d8e24699d61a14af421b19db86
Zadock Maloba <malobazadock@gmail.com>
2022-09-26 13:52:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,23 +9,28 @@ int main(int argc, char *argv[])
 
     auto const config = NaiSys::SystemConfig::readConfigFile();
 
-    for(auto obj : config.value("Enabled-Sites").toArray()){
-        auto const v =obj.toObject();
-        qDebug() << v.value("Name");
-        if(v.value("Ssl").toBool()){
-            auto const serv = new NaiSys::NaiSysHttpsServer(
-                        {v.value("Port").toInt(), config.value("Address").toString()}
-                        );
-            serv->setSslObj({v.value("Ssl-Cert").toString(), v.value("Ssl-Key").toString()});
-            serv->startServer();
-        }
-        else {
-            auto const serv = new NaiSys::NaiSysHttpServer(
-                        {v.value("Port").toInt(), config.value("Address").toString()}
-                        );
-            serv->startServer();
-        }
-    }
+    auto const serv = new NaiSys::NaiSysHttpServer(
+                {1234, "localhost"}
+                );
+    serv->startServer();
+
+//    for(auto obj : config.value("Enabled-Sites").toArray()){
+//        auto const v =obj.toObject();
+//        qDebug() << v.value("Name");
+//        if(v.value("Ssl").toBool()){
+//            auto const serv = new NaiSys::NaiSysHttpsServer(
+//                        {v.value("Port").toInt(), config.value("Address").toString()}
+//                        );
+//            serv->setSslObj({v.value("Ssl-Cert").toString(), v.value("Ssl-Key").toString()});
+//            serv->startServer();
+//        }
+//        else {
+//            auto const serv = new NaiSys::NaiSysHttpServer(
+//                        {v.value("Port").toInt(), config.value("Address").toString()}
+//                        );
+//            serv->startServer();
+//        }
+//    }
 
     return a.exec();
 }
