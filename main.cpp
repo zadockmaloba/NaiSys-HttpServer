86a82a11b71b5dfa5de11bcfbe539cd3b68ddb94
Zadock Maloba <malobazadock@gmail.com>
2022-11-10 17:01:27 +0300

    Create default config entry.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,23 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QString ver = "NaiSys-HttpServer_v0.1.0";
+    NaiSys::SystemConfig::setRootFolderName(ver);
+    if(!NaiSys::SystemConfig::checkForConfigFile()){
+        NaiSys::SystemConfig::insertToConfigFile
+                ("Enabled-Sites", QJsonArray{
+                     QJsonObject{
+                         {"Name", "default-site"},
+                         {"Port", 1234},
+                         {"Address", "localhost"},
+                         {"Ssl", false},
+                         {"Ssl-Cert", ""},
+                         {"Ssl-Key", ""},
+                         {"Entry-Script", "default.nsl"},
+                         {"Site-Dir", NaiSys::SystemConfig::getRootApplicationFolder()}
+                     }
+                 });
+    }
 
     auto const config = NaiSys::SystemConfig::readConfigFile();
 
